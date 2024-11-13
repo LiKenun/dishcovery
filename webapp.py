926e53e525bf899ea77c0a66f2fabb1cb41363bc
Jguan10 <jasoguan10@gmail.com>
2024-11-13 17:00:27 -0500

    Update webapp.py
    Recipes are placed properly now, displaying ingredients, nutrition, and instructions. Recipes are placed in expanders to reduce clutter.

--- webapp.py
+++ webapp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import re
 from sklearn.neighbors import NearestNeighbors
 import scipy.sparse
 import pickle
@@ -40,13 +41,39 @@ def recommend(ingredients_list, top_n=5):
     distances, indices = nearest_neighbors.kneighbors(user_vector, n_neighbors=top_n)
     recommender = data.iloc[indices[0]].copy()
     recommender['Similarity'] = 1 - distances[0]
-    return recommender[['Name', 'Similarity', 'Cleaned_Ingredients']]
+    return recommender
 
 st.title('Dishcovery')
 ingredients_list = st.text_input("Which Ingredients Are You Using?")
 st.write(f'Your Ingredients List is: {ingredients_list}')
 
-with st.spinner('Recommending...'):
-    if st.button('Get Recommendations'):
+# Display each recipe in an expander
+if st.button('Get Recommendations', key = 'Recommendations'):
+    with st.spinner('Recommending...'):
         recommendations = recommend(ingredients_list)
-        st.dataframe(recommendations)
+        for index, row in recommendations.iterrows():
+            with st.expander(row['Name']):
+                st.markdown(f"## {row['Name']}")
+                st.write(f"**Similarity:** {row['Similarity']:.2f}")
+                st.write("**Ingredients:**")
+                st.write(row['Cleaned_Ingredients'])
+
+                # Display nutrition
+                st.write("**Nutrition**")
+                st.write(f"**Calories:** {row['Calories']:.2f}")
+                st.write(f"**Fat:** {row['FatContent']:.2f}g")
+                st.write(f"**Saturated Fat:** {row['SaturatedFatContent']:.2f}g")
+                st.write(f"**Cholesterol:** {row['CholesterolContent']:.2f}mg")
+                st.write(f"**Sodium:** {row['SodiumContent']:.2f}mg")
+                st.write(f"**Total Carbohydrates:** {row['CarbohydrateContent']:.2f}g")
+                st.write(f"**Dietary Fiber:** {row['FiberContent']:.2f}g")
+                st.write(f"**Sugars:** {row['FiberContent']:.2f}g")
+                st.write(f"**Protein:** {row['ProteinContent']:.2f}g")
+                
+                # Parse instructions into separate steps
+                instructions_list = row['Instructions'].split('", "')
+                st.write("**Instructions:**")
+                for i, step in enumerate(instructions_list, 1):
+                    step = step.replace('"', '').replace('[', '').replace(']', '')
+                    st.write(f"{i}. {step}")
+                st.write("------")
